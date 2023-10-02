aeeda34df9ffefea81dbea77ae286d28afa1ad09
James <james@hux.com>
2023-10-02 11:29:20 -0400

    ci

--- rollup.config.js
+++ rollup.config.js
@@ -1,22 +1,15 @@
 import resolve from '@rollup/plugin-node-resolve';
 import multiEntry from '@rollup/plugin-multi-entry';
 import babel from '@rollup/plugin-babel';
-import terser from '@rollup/plugin-terser';
-import filesize from 'rollup-plugin-filesize';
 
 export default {
 	input: 'src/**/*.js',
-	output: [{
+	output: {
 		name: 'poon-ui',
 		file: 'dist/index.js',
 		format: 'esm',
 		generatedCode: {constBindings: true},
-	}, {
-		name: 'poon-ui',
-		file: 'dist/index.min.js',
-		format: 'esm',
-		plugins: [terser()],
-	}],
+	},
 	plugins: [
 		resolve(),
 		babel({
@@ -26,7 +19,6 @@ export default {
 			],
 		}),
 		multiEntry(),
-		filesize(),
 	],
 	external: ['poon-router', 'poon-router/util.js', 'react'],
 };
