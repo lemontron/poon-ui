303618dc15df659a2c65ed9516b8423e1fead299
James <james@hux.com>
2024-05-24 17:04:35 -0400

    update avatar for react 18

--- src/Avatar.js
+++ src/Avatar.js
@@ -1,19 +1,20 @@
 import React from 'react';
 import { c } from './util/index.js';
 
-export const Avatar = ({imageId, className, variant, getUrl, name}) => {
+export const Avatar = ({
+	imageId,
+	className,
+	variant = 'normal',
+	getUrl = val => val,
+	name,
+}) => {
 	if (!imageId) return <div draggable={false} className={c('avatar', className)} title={name}/>;
 	return (
 		<img
 			draggable={false}
 			className={c('avatar', className)}
-			src={getUrl(imageId, variant)}
+			src={imageId && getUrl(imageId, variant)}
 			title={name}
 		/>
 	);
-};
-
-Avatar.defaultProps = {
-	variant: 'normal',
-	getUrl: val => val,
 };
