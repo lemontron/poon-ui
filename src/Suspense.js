caedcd5e22d5e49623ba28a9287ea9548731de38
James <james@hux.com>
2024-08-01 22:29:52 -0400

    rename suspense

--- src/Suspense.js
+++ src/Suspense.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { CStack } from './Stack.js';
 import { ActivityIndicator } from './ActivityIndicator.js';
 
-export const Suspense = () => (
+export const Loading = () => (
 	<CStack>
 		<ActivityIndicator/>
 	</CStack>
